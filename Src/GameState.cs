bbeb6ec3fd67a604768f27e07bcd6d3b09561926
Winded <antton.hytonen@outlook.com>
2024-01-27 16:08:12 +0200

    Starting state

--- Src/GameState.cs
+++ Src/GameState.cs
@@ -1,46 +1,66 @@
+using System.Threading.Tasks;
 using Godot;
 
 public partial class GameState : Node
 {
-	[Signal]
-	public delegate void GameStatusChangeEventHandler(GameStatus oldStatus, GameStatus newStatus);
+    [Signal]
+    public delegate void GameStatusChangeEventHandler(GameStatus oldStatus, GameStatus newStatus);
 
-	[Export]
-	public float MaxShite = 100;
+    [Export]
+    public float MaxShite = 100;
 
-	[Export]
-	public float BaseBuildupSpeed = 2;
+    [Export]
+    public float BaseBuildupSpeed = 2;
 
-	public GameStatus GameStatus => gameStatus;
-	private GameStatus gameStatus = GameStatus.Running;
+    public GameStatus GameStatus => gameStatus;
+    private GameStatus gameStatus = GameStatus.Running;
 
-	public float CurrentShite => shite;
-	private float shite = 0;
+    public float CurrentShite => shite;
+    private float shite = 0;
 
-	public override void _Process(double delta)
-	{
-		if (gameStatus != GameStatus.Running)
-		{
-			return;
-		}
+    public override void _Ready()
+    {
+        SetGameStatus(GameStatus.Starting);
+        var timer = new Timer
+        {
+            Name = "StartTimer",
+            WaitTime = 2f,
+            OneShot = true
+        };
+        AddChild(timer);
+        timer.Timeout += StartGame;
+        timer.Start();
+    }
 
-		shite += BaseBuildupSpeed * (float)delta;
+    public override void _Process(double delta)
+    {
+        if (gameStatus != GameStatus.Running)
+        {
+            return;
+        }
 
-		if (shite >= MaxShite)
-		{
-			SetGameStatus(GameStatus.GameOver);
-		}
-	}
+        shite += BaseBuildupSpeed * (float)delta;
 
-	public void WinTriggerEntered()
-	{
-		SetGameStatus(GameStatus.Victory);
-	}
+        if (shite >= MaxShite)
+        {
+            SetGameStatus(GameStatus.GameOver);
+        }
+    }
 
-	private void SetGameStatus(GameStatus status)
-	{
-		var currentStatus = gameStatus;
-		gameStatus = status;
-		EmitSignal(SignalName.GameStatusChange, (int)currentStatus, (int)gameStatus);
-	}
+    public void WinTriggerEntered()
+    {
+        SetGameStatus(GameStatus.Victory);
+    }
+
+    private void StartGame()
+    {
+        SetGameStatus(GameStatus.Running);
+    }
+
+    private void SetGameStatus(GameStatus status)
+    {
+        var currentStatus = gameStatus;
+        gameStatus = status;
+        EmitSignal(SignalName.GameStatusChange, (int)currentStatus, (int)gameStatus);
+    }
 }
