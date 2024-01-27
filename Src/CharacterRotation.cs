cb2b3a75bd26776157b48f357f0c54c51b0938fd
Max <124260011+timemind@users.noreply.github.com>
2024-01-27 11:25:38 +0200

    Added running sprite. Changed player node from "Sprite2D" to "AnimatedSprite2D" and reflected the change in "CharacterRotation.cs".

--- Src/CharacterRotation.cs
+++ Src/CharacterRotation.cs
@@ -1,23 +1,23 @@
 using Godot;
 
-public partial class CharacterRotation : Sprite2D
+public partial class CharacterRotation : AnimatedSprite2D
 {
-    private CharacterMovement characterMovement;
+	private CharacterMovement characterMovement;
 
-    public override void _Ready()
-    {
-        characterMovement = GetParent<CharacterMovement>();
-    }
+	public override void _Ready()
+	{
+		characterMovement = GetParent<CharacterMovement>();
+	}
 
-    public override void _Process(double delta)
-    {
-        var moveDir = characterMovement.MoveDirection;
-        if (moveDir == Vector2.Zero)
-        {
-            return;
-        }
+	public override void _Process(double delta)
+	{
+		var moveDir = characterMovement.MoveDirection;
+		if (moveDir == Vector2.Zero)
+		{
+			return;
+		}
 
-        var targetAngle = Vector2.Right.AngleTo(moveDir);
-        Rotation = Mathf.LerpAngle(Rotation, targetAngle, 1f / 4f);
-    }
+		var targetAngle = Vector2.Right.AngleTo(moveDir);
+		Rotation = Mathf.LerpAngle(Rotation, targetAngle, 1f / 4f);
+	}
 }
