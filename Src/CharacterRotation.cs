de8535098ef633128f1865013be4fd278dd8add5
Winded <antton.hytonen@outlook.com>
2024-01-27 18:31:57 +0200

    Character look-at rotation

--- Src/CharacterRotation.cs
+++ Src/CharacterRotation.cs
@@ -1,22 +1,60 @@
+using System;
 using Godot;
 
 public partial class CharacterRotation : Node2D
 {
-	private Vector2 moveDir = Vector2.Zero;
+    public enum RotationMode
+    {
+        MoveDirection,
+        LookAtTarget
+    }
 
-	public void MoveDirectionChanged(Vector2 moveDirection)
-	{
-		moveDir = moveDirection;
-	}
+    public RotationMode Mode { get; set; } = RotationMode.MoveDirection;
 
-	public override void _Process(double delta)
-	{
-		if (moveDir == Vector2.Zero)
-		{
-			return;
-		}
+    public Node2D Target { get; set; }
 
-		var targetAngle = Vector2.Right.AngleTo(moveDir);
-		Rotation = Mathf.LerpAngle(Rotation, targetAngle, 1f / 4f);
-	}
+    private Vector2 moveDir = Vector2.Zero;
+
+    public void MoveDirectionChanged(Vector2 moveDirection)
+    {
+        moveDir = moveDirection;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Mode == RotationMode.MoveDirection)
+        {
+            ProcessMoveDir();
+        }
+        else if (Mode == RotationMode.LookAtTarget)
+        {
+            ProcessTarget();
+        }
+    }
+
+    private void ProcessMoveDir()
+    {
+        if (moveDir == Vector2.Zero)
+        {
+            return;
+        }
+
+        RotateTowards(moveDir);
+    }
+
+    private void ProcessTarget()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+
+        RotateTowards((Target.GlobalPosition - GlobalPosition).Normalized());
+    }
+
+    private void RotateTowards(Vector2 direction)
+    {
+        var targetAngle = Vector2.Right.AngleTo(moveDir);
+        Rotation = Mathf.LerpAngle(Rotation, targetAngle, 1f / 4f);
+    }
 }
