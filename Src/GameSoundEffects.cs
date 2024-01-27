8cb225bc0aa412462f91b91c122b9c8ccb144590
Winded <antton.hytonen@outlook.com>
2024-01-27 23:00:43 +0200

    Game state sound effects

---
+++ Src/GameSoundEffects.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+public partial class GameSoundEffects : AudioStreamPlayer
+{
+    [Export]
+    public AudioStream FailureSound;
+
+    public void GameStatusChanged(GameStatus oldStatus, GameStatus newStatus)
+    {
+        Stop();
+        var sound = SoundToPlay(newStatus);
+        if (sound != null)
+        {
+            Stream = sound;
+            Play();
+        }
+    }
+
+    private AudioStream SoundToPlay(GameStatus status)
+    {
+        switch (status)
+        {
+            case GameStatus.GameOver:
+                return FailureSound;
+            default:
+                return null;
+        }
+    }
+}
