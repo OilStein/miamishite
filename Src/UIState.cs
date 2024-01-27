ca546ce3b4ea24fdebe1ba89ff53950140ec9d56
Niko Mac <nikobranart@gmail.com>
2024-01-28 00:29:19 +0200

    Textured Progress bar with color textures, fixed script export

--- Src/UIState.cs
+++ Src/UIState.cs
@@ -2,36 +2,36 @@ using Godot;
 
 public partial class UIState : Node
 {
-    [Export]
-    public GameState GameState;
+	[Export]
+	public GameState GameState;
 
-    [Export]
-    public Control GameRunning;
-    [Export]
-    public Control GameOver;
-    [Export]
-    public Control GameWon;
-    [Export]
-    public Control GameStarting;
+	[Export]
+	public Control GameRunning;
+	[Export]
+	public Control GameOver;
+	[Export]
+	public Control GameWon;
+	[Export]
+	public Control GameStarting;
 
-    [Export]
-    public ProgressBar ShiteProgressBar;
+	[Export]
+	public TextureProgressBar ShiteProgressBar;
 
-    public override void _Process(double delta)
-    {
-        ShiteProgressBar.Value = GameState.CurrentShite / GameState.MaxShite * 100f;
-    }
+	public override void _Process(double delta)
+	{
+		ShiteProgressBar.Value = GameState.CurrentShite / GameState.MaxShite * 100f;
+	}
 
-    public void _GameStatusChanged(int oldStatus, int newStatus)
-    {
-        OnGameStatusChanged((GameStatus)oldStatus, (GameStatus)newStatus);
-    }
+	public void _GameStatusChanged(int oldStatus, int newStatus)
+	{
+		OnGameStatusChanged((GameStatus)oldStatus, (GameStatus)newStatus);
+	}
 
-    private void OnGameStatusChanged(GameStatus oldStatus, GameStatus newStatus)
-    {
-        GameRunning.Visible = newStatus == GameStatus.Running;
-        GameOver.Visible = newStatus == GameStatus.GameOver;
-        GameWon.Visible = newStatus == GameStatus.Victory;
-        GameStarting.Visible = newStatus == GameStatus.Starting;
-    }
+	private void OnGameStatusChanged(GameStatus oldStatus, GameStatus newStatus)
+	{
+		GameRunning.Visible = newStatus == GameStatus.Running;
+		GameOver.Visible = newStatus == GameStatus.GameOver;
+		GameWon.Visible = newStatus == GameStatus.Victory;
+		GameStarting.Visible = newStatus == GameStatus.Starting;
+	}
 }
