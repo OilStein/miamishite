0e15bd13540c73db4d54b26aa16ab4a9c3d657ab
Niko Mac <nikobranart@gmail.com>
2024-01-28 13:06:20 +0200

    Farted method name changed to Fart

--- Src/GameState.cs
+++ Src/GameState.cs
@@ -3,71 +3,71 @@ using Godot;
 
 public partial class GameState : Node
 {
-    [Signal]
-    public delegate void GameStatusChangeEventHandler(GameStatus oldStatus, GameStatus newStatus);
+	[Signal]
+	public delegate void GameStatusChangeEventHandler(GameStatus oldStatus, GameStatus newStatus);
 
-    [Signal]
-    public delegate void FartedEventHandler();
+	[Signal]
+	public delegate void FartedEventHandler();
 
-    [Export]
-    public float MaxShite = 100;
+	[Export]
+	public float MaxShite = 100;
 
-    [Export]
-    public float BaseBuildupSpeed = 2;
+	[Export]
+	public float BaseBuildupSpeed = 2;
 
-    public GameStatus GameStatus => gameStatus;
-    private GameStatus gameStatus = GameStatus.Running;
+	public GameStatus GameStatus => gameStatus;
+	private GameStatus gameStatus = GameStatus.Running;
 
-    public float CurrentShite => shite;
-    private float shite = 0;
+	public float CurrentShite => shite;
+	private float shite = 0;
 
-    public override void _Ready()
-    {
-        SetGameStatus(GameStatus.Starting);
-        var timer = new Timer
-        {
-            Name = "StartTimer",
-            WaitTime = 2f,
-            OneShot = true
-        };
-        AddChild(timer);
-        timer.Timeout += StartGame;
-        timer.Start();
-    }
+	public override void _Ready()
+	{
+		SetGameStatus(GameStatus.Starting);
+		var timer = new Timer
+		{
+			Name = "StartTimer",
+			WaitTime = 2f,
+			OneShot = true
+		};
+		AddChild(timer);
+		timer.Timeout += StartGame;
+		timer.Start();
+	}
 
-    public override void _Process(double delta)
-    {
-        if (gameStatus != GameStatus.Running)
-        {
-            return;
-        }
+	public override void _Process(double delta)
+	{
+		if (gameStatus != GameStatus.Running)
+		{
+			return;
+		}
 
-        shite += BaseBuildupSpeed * (float)delta;
+		shite += BaseBuildupSpeed * (float)delta;
 
-        if (shite >= MaxShite)
-        {
-            SetGameStatus(GameStatus.GameOver);
-        }
-    }
+		if (shite >= MaxShite)
+		{
+			SetGameStatus(GameStatus.GameOver);
+		}
+	}
 
-    public void WinTriggerEntered()
-    {
-        SetGameStatus(GameStatus.Victory);
-    }
+	public void WinTriggerEntered()
+	{
+		SetGameStatus(GameStatus.Victory);
+	}
 
-    private void StartGame()
-    {
-        SetGameStatus(GameStatus.Running);
-    }
+	private void StartGame()
+	{
+		SetGameStatus(GameStatus.Running);
+	}
 
-    private void SetGameStatus(GameStatus status)
-    {
-        var currentStatus = gameStatus;
-        gameStatus = status;
-        EmitSignal(SignalName.GameStatusChange, (int)currentStatus, (int)gameStatus);
-    }
+	private void SetGameStatus(GameStatus status)
+	{
+		var currentStatus = gameStatus;
+		gameStatus = status;
+		EmitSignal(SignalName.GameStatusChange, (int)currentStatus, (int)gameStatus);
+	}
 
-	private void Farted()
+	private void Fart()
 	{
 		var rand = new System.Random();
 		var didAddOrRemoveShite = rand.Next(0, 4);
@@ -83,6 +83,6 @@ public partial class GameState : Node
 		{
 			shite = 0;
 		}
-        EmitSignal(SignalName.Farted);
+		EmitSignal(SignalName.Farted);
 	}
 }
