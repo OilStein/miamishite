02f5e869f7eec7d599b459059a90b2a2163fc44b
Winded <antton.hytonen@outlook.com>
2024-01-28 13:13:23 +0200

    Death animation, main scene building

--- Src/CharacterAnimations.cs
+++ Src/CharacterAnimations.cs
@@ -2,8 +2,15 @@ using Godot;
 
 public partial class CharacterAnimations : AnimatedSprite2D
 {
+    private GameStatus gameStatus = GameStatus.Starting;
+
     public void MoveDirectionChanged(Vector2 moveDirection)
     {
+        if (gameStatus != GameStatus.Running)
+        {
+            return;
+        }
+
         if (moveDirection == Vector2.Zero)
         {
             Play("idle");
@@ -11,6 +18,19 @@ public partial class CharacterAnimations : AnimatedSprite2D
         else
         {
             Play("run");
+        }
+    }
+
+    public void GameStatusChanged(GameStatus oldState, GameStatus newState)
+    {
+        gameStatus = newState;
+        if (gameStatus == GameStatus.GameOver)
+        {
+            Play("death");
+        }
+        else
+        {
+            Play("idle");
         }
     }
 }
